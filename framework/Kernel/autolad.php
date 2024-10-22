420dd6ffdd9f705fd9e844551057218a6858ecb3
guiurm <guiurm@gmail.com>
2024-10-22 12:58:11 +0200

    fix: autoload

--- framework/Kernel/autolad.php
+++ framework/Kernel/autolad.php
@@ -2,19 +2,36 @@
 
 namespace Src\Kernel;
 
+// /framework/Kernel/autoload.php
 $root = __DIR__;
 
 $position = strpos(__DIR__, 'framework');
 
-if ($position !== false)
+if ($position !== false) {
     $root = substr(__DIR__, 0, $position);
+}
 
 define('__ROOT__', $root);
-spl_autoload_register(function ($class_name) {
 
+spl_autoload_register(function ($class_name) {
+    // Convertimos el namespace a una ruta de archivo
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $class_name) . '.php';
-    require_once __ROOT__ . $file;
+
+    // Intentamos incluir el archivo
+    $full_path = __ROOT__ . DIRECTORY_SEPARATOR . $file;
+
+    if (file_exists($full_path)) {
+        require_once $full_path;
+    } else {
+        throw new \RuntimeException("Unable to load class: $class_name. File not found: $full_path");
+    }
 });
+
+// Manejo de excepciones global
+set_exception_handler(function ($exception) {
+    echo "Error: " . $exception->getMessage();
+});
+
 
 function dd($data)
 {
